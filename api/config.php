0f56771b1a7f6d34464f90c923fe94ee2faa76ed
Brandon0802 <54695404+Brandon0802@users.noreply.github.com>
2023-01-04 06:24:09 +0800

    Update config.php

--- api/config.php
+++ api/config.php
@@ -1,13 +1,12 @@
 <?php
-  header('Access-Control-Allow-Origin: *');
-    define('DB_NAME', 'capstone_project');
-    define('DB_USER', 'root');
-    define('DB_PASSWORD', '');
-    define('DB_HOST', 'localhost');
-    //define('DB_NAME', 'sql6582081');
-    //define('DB_USER', 'sql6582081');
-    //define('DB_PASSWORD', 'iGk7qG21rH');
-    //define('DB_HOST', 'sql6.freemysqlhosting.net');
+    //define('DB_NAME', 'capstone_project');
+    //define('DB_USER', 'root');
+    //define('DB_PASSWORD', '');
+    //define('DB_HOST', 'localhost');
+    define('DB_NAME', 'sql6588064');
+    define('DB_USER', 'sql6588064');
+    define('DB_PASSWORD', 'jSTnxGY7QX');
+    define('DB_HOST', 'sql6.freemysqlhosting.net');
 
 
     $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
